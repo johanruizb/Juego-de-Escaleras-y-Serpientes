35017d011c9d7166a554c89e38b2fd295d31114c
Victor Alomia <alomia18@gmail.com>
2021-09-03 20:53:57 -0500

    se realiza reset al proyecto

--- src/escalerasSerpientes/Principal.java
+++ src/escalerasSerpientes/Principal.java
@@ -5,13 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Prueba ");
-		System.out.println("Prueba ");
-		System.out.println("Prueba ");
-
-		System.out.println("Prueba 23");
-		int x = 128;
-		double xl = 428;
+		
 
 	}
 

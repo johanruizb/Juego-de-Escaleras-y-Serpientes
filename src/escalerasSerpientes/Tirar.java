8f5d3142513d9e64f6c5401474535035dd149c88
Victor Alomia <alomia18@gmail.com>
2021-09-10 00:23:55 -0500

    se incorpora la clase de audio, la clase de audio sonido esta funcional la clase MediaPlay esta en construccion no funciona

---
+++ src/escalerasSerpientes/Tirar.java
@@ -0,0 +1,42 @@
+package escalerasSerpientes;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Tirar extends JFrame {
+	private Sonido sonido;
+	private MediaPlay play;
+	public Tirar() {
+		initGUI();
+		this.setTitle("Esacaleras y Serpientes");
+		this.setVisible(true);
+		this.setSize(200, 100);
+		this.setLocationRelativeTo(null);
+//		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	private void initGUI() {
+		// TODO Auto-generated method stub
+		sonido = new Sonido();
+		play = new MediaPlay();
+		JButton b = new JButton("Reproducir");
+		b.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+//				sonido.reproductorMusic("audio1");
+			play.MediaPlay("audio2.mp3");
+				
+			}
+			
+		});
+		add(b,BorderLayout.CENTER);
+	}
+
+}
